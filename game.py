e511c3059e4e9310c75140bd689ae9cf1beab461
danil <danil.vladimirow2016@yandex.ru>
2022-01-22 23:50:18 +0300

    оформил меню как функцию, убрал лишнее

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-def browse(txt_name='settings.txt'):
+def browse_glob_data(txt_name='settings.txt'):
     '''функция для получения данных из текстовика в форме словаря
     :return dict'''
     ss = {}
@@ -9,6 +9,36 @@ def browse(txt_name='settings.txt'):
         except ValueError:
             ss[item[0]] = item[1]
     return ss
+
+def save_glob_data(thing, txt_name='settings.txt'):
+    '''фенкция для сохранения словаря/котрежа/списка в текстовик
+    :arg thing - что сохранить'''
+    for i in thing.items:
+        print(i)
+
+def start_menu(nick, g_modes):
+    import pygame
+    import pygame_menu
+
+    def set_difficulty(value, difficulty):
+        global diff
+        diff = 1
+        diff = difficulty
+
+    def start_the_game():
+        user_age = text_input.get_value()
+        print(user_age, diff)
+
+    diff = 1
+    pygame.init()
+    surface = pygame.display.set_mode((800, 450))
+
+    menu = pygame_menu.Menu('Welcome', 800, 450, theme=pygame_menu.themes.THEME_BLUE)
+    text_input = menu.add.text_input('Никнейм :', default=nick, maxchar=30)
+    menu.add.selector('Режим игры:', g_modes, onchange=set_difficulty)
+    menu.add.button('Играть', start_the_game)
+    menu.add.button('Выход', pygame_menu.events.EXIT)
+    menu.mainloop(surface)
 
 
 
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pygame
 import pygame_menu
 from game import *
 
-
+'''----------------globals---------------'''
+global xy
+glob_data = browse_glob_data()
+xy = (glob_data['x'], glob_data['y'])
+nick = glob_data['last_nick']
+g_modes = [('Standart', 1), ('Advansed', 2)]
 
 '''class Board:
     def __init__(self, width, height):
@@ -54,32 +59,9 @@ except Exception as e:
 pygame.quit()
 '''
 
-def set_difficulty(value, difficulty):
-    # Do the job here !
-    pass
-
-
-def start_the_game():
-    user_age = nick.get_value()
-    diff = diff.get_value()
-    print(user_age)  # to print the value of user_age, only for debugging tho, don't use this when you have finished the game, because user is not really supposed to read the console anyways
-
-    # rest of the game code
+start_menu(nick, g_modes)
 
 
 
-glob_sets = browse()
-x, y = glob_sets['x'], glob_sets['y']
-g_modes = [('Standart', 1), ('Advansed', 2)]
-
-pygame.init()
-surface = pygame.display.set_mode((x, y))
-
-menu = pygame_menu.Menu('Welcume', x, y,theme=pygame_menu.themes.THEME_BLUE)
-nick = menu.add.text_input('Никнейм :', default=glob_sets['last_nick'], maxchar=30)
-diff = menu.add.selector('Режим игры:', g_modes, )
 
 
-menu.add.button('Играть', start_the_game)
-menu.add.button('Выход', pygame_menu.events.EXIT)
-menu.mainloop(surface)
